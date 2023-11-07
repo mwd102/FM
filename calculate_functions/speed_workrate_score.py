d03d02e2cc9ba5d4c6151d6f5021cee32ebcf234
Fatheed7 <dunton.jason@gmail.com>
2023-11-07 16:37:11 +0000

    Fix: Syntax error in speed_workrate_score.py

--- calculate_functions/speed_workrate_score.py
+++ calculate_functions/speed_workrate_score.py
@@ -1,8 +1,8 @@
 def calculate_speed_workrate_score(squad_rawdata):
     squad_rawdata['Spd'] = (
         squad_rawdata['Pac'] + squad_rawdata['Acc']) / 2
-    squad_rawdata['Work'] =
-    (squad_rawdata['Wor'] + squad_rawdata['Sta']) / 2
+    squad_rawdata['Work'] = (
+        squad_rawdata['Wor'] + squad_rawdata['Sta']) / 2
     squad_rawdata['SetP'] = (
         squad_rawdata['Jum'] + squad_rawdata['Bra']) / 2
     return squad_rawdata
